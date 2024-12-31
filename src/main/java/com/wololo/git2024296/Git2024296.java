da86591b9c037274a6e7e2d328d32e6dcb325509
Papelmache296 <2024296@student.cct.ie>
2024-12-31 01:13:55 +0000

    (empty!)

---
+++ src/main/java/com/wololo/git2024296/Git2024296.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.wololo.git2024296;
+
+/**
+ *
+ * @author sanch
+ */
+public class Git2024296 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
